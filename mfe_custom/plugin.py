6407afc86f60e1fb8dbf2ca2edbd46e5e3c96924
hesam <amirhesam.moradi73@gmail.com>
2026-02-15 02:32:08 +0330

    3rd

--- mfe_custom/plugin.py
+++ mfe_custom/plugin.py
@@ -1,27 +1,62 @@
 from tutor import hooks
 
-hooks.Filters.ENV_PATCHES.add_items(
-    [
-        (
-            "mfe-dockerfile-post-install",
-            """
-# Create directories
-RUN mkdir -p /openedx/app/public/fonts
-RUN mkdir -p /openedx/app/public/assets
-
-# Copy fonts
-COPY ./plugins/mfe_custom/Vazirmatn-Regular.woff2 /openedx/app/public/fonts/
-COPY ./plugins/mfe_custom/Vazirmatn-Bold.woff2 /openedx/app/public/fonts/
-
-# Copy logo
-COPY ./plugins/mfe_custom/logo.png /openedx/app/public/assets/logo.png
-
-# Copy SCSS
-COPY ./plugins/mfe_custom/mfe_custom.scss /openedx/app/src/mfe_custom.scss
-
-# Override Footer
-COPY ./plugins/mfe_custom/footer_override.js /openedx/app/src/components/Footer/index.jsx
+hooks.Filters.ENV_PATCHES.add_items([
+    # Override logos and favicon
+    (
+        "mfe-lms-development-settings",
+        """
+MFE_CONFIG["LOGO_URL"] = "/assets/logo.png"
+MFE_CONFIG["FAVICON_URL"] = "/assets/logo.png"
 """
+    ),
+    (
+        "mfe-lms-production-settings",
+        """
+MFE_CONFIG["LOGO_URL"] = "/assets/logo.png"
+MFE_CONFIG["FAVICON_URL"] = "/assets/logo.png"
+"""
+    ),
+])
+
+# Frontend Plugin Slot برای Footer
+hooks.Filters.MFE_FRONTEND_PLUGIN_SLOTS.add_items([
+    (
+        "all",
+        "footer_slot",
+        """
+{
+    op: PLUGIN_OPERATIONS.Hide,
+    widgetId: 'default_contents',
+}
+"""
+    ),
+    (
+        "all",
+        "footer_slot",
+        """
+{
+    op: PLUGIN_OPERATIONS.Insert,
+    widget: {
+        id: 'custom_footer',
+        type: DIRECT_PLUGIN,
+        RenderWidget: () => (
+            <footer className='custom-footer'>
+                <div style={{textAlign: 'center', padding: '20px'}}>
+                    <img src='/assets/logo.png' alt='University Logo' height='60'/>
+                    <div style={{marginTop: '10px'}}>
+                        <a href='/about'>درباره ما</a> | 
+                        <a href='/contact'>تماس با ما</a> | 
+                        <a href='/privacy'>حریم خصوصی</a> | 
+                        <a href='/terms'>قوانین استفاده</a>
+                    </div>
+                    <div style={{marginTop: '10px'}}>
+                        © 2026 سامانه آموزش الکترونیکی
+                    </div>
+                </div>
+            </footer>
         ),
-    ]
-)
+    },
+}
+"""
+    )
+])
